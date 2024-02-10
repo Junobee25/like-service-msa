a3b96f115b10445e4715249fc24249e9836e6d5e
Junobee25 <wnsdhqo@naver.com>
2024-02-10 16:02:19 +0900

    #9 - 좋아요, 상호 작용 알림 간 FeignClient 구현

--- src/main/java/com/hanghae/likeservice/external/client/FeedServiceClient.java
+++ src/main/java/com/hanghae/likeservice/external/client/FeedServiceClient.java
@@ -4,10 +4,19 @@ import com.hanghae.likeservice.domain.constant.LikeType;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @FeignClient(name="feed-service")
 public interface FeedServiceClient {
 
     @GetMapping("/feed-service/{likeType}/{targetId}")
     Boolean hasTarget(@PathVariable LikeType likeType, @PathVariable Long targetId);
+
+    @GetMapping("/feed-service/posts/find-user-id")
+    Optional<Long> getPostUserId(@RequestParam(value = "targetId", required = false) Long targetId);
+
+    @GetMapping("/feed-service/comments/find-uesr-id")
+    Optional<Long> getCommentUserId(@RequestParam(value = "targetId", required = false) Long targetId);
 }
